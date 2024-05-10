c6c7926f0196e24fd61aeb464b2bb66b2a5269c6
mateusisrael <mateuss.i.fernandes@gmail.com>
2024-05-10 16:37:47 -0300

    chore(typeORM): config typeorm

---
+++ src/database/index.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+
+export const myDataSource = new DataSource({
+  type: 'postgres',
+  host: 'localhost',
+  port: 3306,
+  username: 'test',
+  password: 'test',
+  database: 'test',
+  logging: true,
+  synchronize: true,
+  entities: [
+    './src/modules/Match/entities/*.ts',
+    './src/modules/Team/entities/*.ts',
+  ],
+  migrations: ['./src/database/migrations/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+});
